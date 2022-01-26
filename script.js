f18369c0d288c370cfd7bc8c94b4e21677c67d44
Ezequiel Faustino <ezequielfaustino@gmail.com>
2022-01-25 21:52:52 -0400

    mode script

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const calculate = document.getElementById('calculate'); //pega o botao Calcular e salva na const
 
-
-
 function calculateIMC(){
     const name = document.getElementById('name').value; //pega o nome e salva na const
     const height = document.getElementById('height').value; //pega a altura e salva na const
@@ -10,11 +8,32 @@ function calculateIMC(){
 
     //validacao para ver se os campos estao preenchidos
     if(name !=="" && height !=="" && weight !==""){
-        alert('todos os campos preenchidos!!')
+        const valorIMC = parseFloat( ( weight / (height * height))).toFixed(1);
+        let classificacao = '';
+        if(valorIMC < 18.5){
+            classificacao = 'abaixo do peso!!'
+        }
+        else if(valorIMC < 25){
+            classificacao = 'com peso ideal!!'
+        }
+        else if(valorIMC < 30){
+            classificacao = 'levemente acima do peso!!'
+        }
+        else if(valorIMC < 35){
+            classificacao = 'com obesidade grau I'
+        }
+        else if(valorIMC < 40){
+            classificacao = 'com obesidade grau II'
+        }
+        else{
+            classificacao = 'com obesidade grau III!!'
+        }
+        result.textContent = `${name}, seu IMC é: ${valorIMC} e voce esta ${classificacao}`; //joga o calculo do IMC para a div result
     }else{
-        result.textContent = 'Preencha todos os campos!!' //escrever dentro do conteudo da div
+        let string = '';
+        string+= 'Ex Altura: 1.75'+'\n'
+        string+= '\n'+'Preencha todos os campos!!'
+        result.textContent = string //escrever dentro do conteudo da div
     }
 };
-
-
 calculate.addEventListener('click', calculateIMC);
